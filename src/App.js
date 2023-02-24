8cb4f6aac3c3272ed37c2ef3932376e4f483f34d
Ajila K Johnkutty <kjajila@gmail.com>
2023-02-23 20:01:25 -0500

    add Footer component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Header from "./components/Header";
 import AddContact from "./components/AddContact";
 // import ContactCard from "./components/ContactCard";
 import ContactList from "./components/ContactList";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="ui container">
         <Header/>
         <AddContact/>
-        {/* <ContactList/> */}
-
-       <h2>React project from scratch</h2>
+        <ContactList/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h4>React project from scratch</h4>
+    </div>
+  )
+}
+
+export default Footer
