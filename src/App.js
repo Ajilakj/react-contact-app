0057dac5882749802aaefcaa2e973f9af8a69e47
Ajila K Johnkutty <kjajila@gmail.com>
2023-02-23 21:24:31 -0500

    add ContactCard component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import './App.css';
 import Header from "./components/Header";
 import AddContact from "./components/AddContact";
-// import ContactCard from "./components/ContactCard";
 import ContactList from "./components/ContactList";
 import Footer from "./components/Footer";
 
