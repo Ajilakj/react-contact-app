34920de0642f7592b3d182a03897877e09ace781
Ajila K Johnkutty <kjajila@gmail.com>
2023-02-23 22:30:30 -0500

    add useState hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 // import './App.css';
+import React ,{useState} from 'react'
 import Header from "./components/Header";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
 import Footer from "./components/Footer";
 
 function App() {
-  const contacts=[
-    {
-      id:1,
-      "name":"Ashlyn",
-      "email":"ashlynbobby@gmail.com"
-    },
-    {
-      id:2,
-      "name":"Abigail",
-      "email":"abigailbobby@gmail.com"
-    },
-  ];
+  // const contacts=[
+  //   {
+  //     id:1,
+  //     "name":"Ashlyn",
+  //     "email":"ashlynbobby@gmail.com"
+  //   },
+  //   {
+  //     id:2,
+  //     "name":"Abigail",
+  //     "email":"abigailbobby@gmail.com"
+  //   },
+  // ];
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) =>{
+    console.log(contact);
+  }
   return (
     <div className="ui container">
         <Header/>
-        <AddContact/>
+        <AddContact addContactHandler={addContactHandler}/>
         <ContactList contacts={contacts}/>
         <Footer/>
     </div>
--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,22 +1,45 @@
 import React from 'react'
 
-const AddContact = () => {
+class AddContact extends React.Component{
+  state={
+    name:"",
+    email:""
+  };
+
+  add= (e) =>{
+    e.preventDefault();
+    if(this.state.name === "" || this.state.email === ""){
+      alert("All the fields are mandatory");
+      return;
+    }
+    this.props.addContactHandler(this.state);
+    this.setState({name:"", email:""})
+  }
+
+  render(){ // when we use class we have to wrap return in rende function
   return (
     <div className="ui main">
         <h2>Add Contact</h2>
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.add}>
             <div className="field">
                 <label for = "name">Name</label> 
-                <input type="text" name="name" placeholder="name"/>
+                <input type="text" name="name" placeholder="name"
+                value={this.state.name}
+                onChange={(e) => this.setState({name:e.target.value})}
+                />
             </div>
             <div className="field">
                 <label for = "email">email</label> 
-                <input type="text" name="email" placeholder="email"/>
+                <input type="text" name="email" placeholder="email"
+                value={this.state.email}
+                onChange={(e) => this.setState({email:e.target.value})}
+                />
             </div>
             <button className="ui buttton blue">Add</button>
         </form>
     </div>
   )
+  }
 }
 
 export default AddContact
